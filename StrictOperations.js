09ea7f2c1a711cf6700eb3075ee03edb6f345c4e
Aniket Patil <55218425+aniketpatil03@users.noreply.github.com>
2022-09-03 10:26:04 +0530

    Update and rename StrictMatch.js to StrictOperations.js

---
+++ StrictOperations.js
@@ -0,0 +1,7 @@
+let string1 = "  aniket patil  "
+console.log(string1)
+console.log("Frst occurance of a : " + string1.indexOf("a"))
+console.log("last occurance of a : " + string1.lastIndexOf("a"))
+console.log("substring a : " + string1.substring(2, 10))
+console.log("trim :" + string1.trim())
+console.log(string1.toUppperCase + string1.toLowerCase)
