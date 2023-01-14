43157290acac96be9ef0230de8dc8fa9eca1482f
Aniket Patil <55218425+aniketpatil03@users.noreply.github.com>
2023-01-14 23:53:58 +0530

    Create Udemy_ExpressionVsStatement.js

---
+++ Udemy_ExpressionVsStatement.js
@@ -0,0 +1,12 @@
+//Anything that produces a value is a expression, if does not it is a Statement
+
+4 + 8
+1991 
+true && false && !false
+// all exmples above produces value hence it's a expression
+
+if (10 > 67) {
+    const str = "10 is larger";
+}
+
+// Above if does not produce a value hence it's a statement
